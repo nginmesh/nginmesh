abc9446a635b6170764289329b1ce0153bf5cc12
sakows <SAKO@nginx.com>
2018-04-20 12:03:12 -0700

    Update Zipkin Test case

--- istio/tests/cases/11_zipkin_spec.py
+++ istio/tests/cases/11_zipkin_spec.py
@@ -10,14 +10,7 @@ with description('Zipkin tracing functionality'):
          #Read Config file
          configuration.setenv(self)
 
-    with context('Set environment'):
-         with it('Add Zipkin tracing feature'):
-            configuration.run_shell("kubectl apply -f "+configuration.nginmesh_install_path+"zipkin/zipkin.yaml","run")
-            time.sleep(configuration.rule_apply_time)
-            configuration.run_shell("kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=zipkin -o jsonpath='{.items[0].metadata.name}') 9411:9411 &","run")
-            time.sleep(configuration.rule_apply_time)
-
-    with context('Starting Test'):
+    with context('Deploy Zipkin and make sure port forwarded to localhost'):
         with it('Bookinfo Zipkin tracing feature'):
             for _ in range(10):
                 r = requests.get(self.url)
@@ -31,11 +24,6 @@ with description('Zipkin tracing functionality'):
             else:
                 expect(0).not_to(equal(0))
             configuration.generate_request(self)
-
-    with context('Clean Environment'):
-        with it('Delete Zipkin tracing feature'):
-            configuration.run_shell("kubectl delete -f "+configuration.nginmesh_install_path+"zipkin/zipkin.yaml","run")
-            configuration.run_shell("ps -ef | grep zipkin | grep -v grep | awk '{print $2}' | xargs kill -9","run")
 
 
 
