c16e6afe0f612ec96f04bac5da18bac12d6812e2
Aidan Carson <aidan.carson@nginx.com>
2018-07-16 10:31:37 -0700

    Update to Retrieve Mixer Module

--- istio/agent/modules.mk
+++ istio/agent/modules.mk
@@ -3,7 +3,7 @@ MIXER_MODULE_PROJ_NAME=ngx-istio-mixer
 MIXER_MODULE_NAME=ngx_http_istio_mixer_module
 DEST_MODULE_PROJ_NAME=ngx-stream-nginmesh-dest
 DEST_MODULE_NAME=ngx_stream_nginmesh_dest_module
-MIXER_VERSION=0.2.12-RC2
+MIXER_VERSION=0.7.1
 DEST_VERSION=0.2.12-RC2
 COPY_MODULE=release
 
@@ -17,7 +17,7 @@ modules-dirs:
 
 modules: tracing-modules
 	cd $(BUILD_DIR)/modules; \
-	# wget -N https://github.com/nginmesh/${MIXER_MODULE_PROJ_NAME}/releases/download/${MIXER_VERSION}/${MIXER_MODULE_NAME}.so; \
+	wget -N https://github.com/nginmesh/${MIXER_MODULE_PROJ_NAME}/releases/download/${MIXER_VERSION}/${MIXER_MODULE_NAME}.so; \
 	wget -N https://github.com/nginmesh/${DEST_MODULE_PROJ_NAME}/releases/download/${DEST_VERSION}/${DEST_MODULE_NAME}.so
 
 container-tracing-modules-builder:
